5389d41b2313d77323e7fc36e56adf482be58482
Baba-Aruj <aruj877@gmail.com>
2022-11-24 09:44:58 +0300

    test

--- src/test/java/io/duqow/app/restAPI/RestApiApplicationTests.java
+++ src/test/java/io/duqow/app/restAPI/RestApiApplicationTests.java
@@ -2,12 +2,16 @@ package io.duqow.app.restAPI;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.context.ApplicationContext;
+
 
 @SpringBootTest
 class RestApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
 	}
 
 }
